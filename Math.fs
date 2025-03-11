c3a0f0926f772394369a56965029de4f3cb30bb5
zerojjun <kbg9787@naver.com>
2025-03-11 10:12:35 +0900

    Quiz 2 complete

--- Math.fs
+++ Math.fs
@@ -5,5 +5,14 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let collatz n =
-  0
+
+
+let f n =
+    if n % 2UL = 0UL then n / 2UL else 3UL * n + 1UL
+
+let collatz (n: int) =
+    let rec iter cnt n =
+        let result = f n
+        if result = 1UL then cnt else iter (cnt + 1) result
+
+    iter 1 (uint64 n)
--- Program.fs
+++ Program.fs
@@ -4,8 +4,8 @@ open CS220.Quiz1.Math
 
 [<EntryPoint>]
 let main _args =
-  printfn "You can test your program by directly modifying Program.fs"
-  printfn "Or, you can change the unit tests in the `tests` directory."
-  printfn "---"
-  collatz 4 |> printfn "# of steps to reach one = %d"
-  0
+    printfn "You can test your program by directly modifying Program.fs"
+    printfn "Or, you can change the unit tests in the `tests` directory."
+    printfn "---"
+    collatz 4 |> printfn "# of steps to reach one = %d"
+    0
